22105559048325c7babf51e974ef677a6ae14d8f
Rinat <dieselok161@gmail.com>
2024-09-19 11:29:40 +0300

    Update flow_vm_test.dart

--- lib/flow_vm_test.dart
+++ lib/flow_vm_test.dart
@@ -1,7 +1,3 @@
 library flow_vm_test;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:flow_vm_test/src/view_model_test.dart';
