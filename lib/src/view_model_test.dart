7cc616584fb48a8eb84cf2b306eaa865006e678b
Rinat <dieselok161@gmail.com>
2024-09-19 12:25:33 +0500

    init

---
+++ lib/src/view_model_test.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+
+import 'package:flow_vm/flow_vm.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:meta/meta.dart';
+
+@isTest
+void viewModelTest(
+  String description, {
+  required Iterable<FlowTestCase> Function() cases,
+  required Future Function() action,
+  FutureOr Function()? verify,
+  dynamic Function()? setUp,
+}) {
+  _viewModelTest(
+    description,
+    casesBuilder: cases,
+    action: action,
+    verify: verify,
+    setUpFun: setUp,
+  );
+}
+
+void _viewModelTest(
+  String description, {
+  required Iterable<FlowTestCase> Function() casesBuilder,
+  required Future Function() action,
+  required FutureOr Function()? verify,
+  required dynamic Function()? setUpFun,
+}) {
+  test(
+    description,
+    () async {
+      setUpFun?.call();
+
+      final cases = casesBuilder();
+      for (var element in cases) {
+        element._start();
+      }
+      await action();
+      for (var element in cases) {
+        element._finish();
+      }
+      await verify?.call();
+    },
+  );
+}
+
+class FlowTestCase<V> {
+  FlowTestCase({
+    required this.flow,
+    required Iterable<V> expect,
+  }) : _expect = expect;
+
+  final DataFlow<V> flow;
+  final Iterable<V> _expect;
+  late final received = <V>[flow.value];
+
+  void _listener() {
+    received.add(flow.value);
+  }
+
+  void _start() {
+    flow.listenable.addListener(_listener);
+  }
+
+  void _finish() {
+    flow.listenable.removeListener(_listener);
+    return expect(received, _expect);
+  }
+}
